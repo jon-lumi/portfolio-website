c0e95715e13668de467d0b0fde034c12f7a7a22f
Jon Lumi <jlumi@debian.mshome.net>
2024-04-21 13:45:37 +0200

    Changed the home page design back.

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,44 +1,17 @@
-import Image from "next/image";
-import me from "./../images/me.png"
-import { description } from "../data";
-
 export default function Home() {
 	return (
-		<main 
-			className="flex-1 flex justify-center items-center
-								flex-col-reverse gap-unit-lg
-								md:flex-row md:gap-unit-3xl"
-			
-		>
-			<div
-				className="flex flex-col justify-center
-									w-full p-unit-lg items-center
-									gap-unit-xs md:w-2/5 md:items-start"
+		<main	className="flex-1 flex flex-col justify-center items-center">
+			<h1
+				className="text-text font-bold w-fit
+										text-5xl
+										md:text-7xl"
 			>
-				<h1
-					className="text-text font-bold w-fit
-											text-4xl
-											md:text-7xl"
-				>
-					Jon Lumi
-				</h1>
-				<div
-					className="flex flex-col gap-unit-xs"
-				>
-					{description.split("\n").map((line, index) => (
-						<p key={index} className="text-primary 
-																			text-sm leading-6
-																			md:text-lg md:leading-7">
-							{line}
-						</p>
-					))}
-				</div>
-			</div>
-
-			<img
-				src={me.src}
-				className="size-unit-5xl md:size-unit-6xl"
-			/>
+				Jon Lumi
+			</h1>
+			<p className="text-primary text-sm md:text-lg text-center px-unit-md"
+			>
+				I like making things!
+			</p>
 		</main>
 	)
 }
